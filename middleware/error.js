bc62acc3115bfd79a22f1c62df422b525a892bc5
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 17:40:01 -0600

    Add the functionality to pick custom errors and status codes

--- middleware/error.js
+++ middleware/error.js
@@ -1,12 +1,18 @@
-
+const ErrorRes = require('../utilities/error');
 
  const errorHandler = (err, req, res, next) => {
+// create a variable that inherits all properties of err with spread operators
+  let error = {...err};
 
-    console.log(err.stack.red.bold);
+  error.message = err.message;
+
+    console.log(error);
+    // console.log(err.stack.red.bold);
+
     
-    res.status(500).json({
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message
+        error: error.message || 'Server Error'
     })
   }
   
