d3dd1c42349e3ed6e01c85abb8868ea41b46dd0a
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-23 14:29:37 -0600

    Middleware that decodes the web token and protests the route

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+
+const ErrorRes = require('../utilities/error');
+const Users = require('../models/Users');
+
+
+dotenv.config({path: '../config/config.env'})
+// middle ware to protect the routes
+
+exports.protectedRoutes = asyncHandler(async (req, res, next)=>{
+  let token;
+ // Check for the authorization header
+ if(req.headers.authorization && req.headers.authorization.startsWith('Baerer')){
+   token = req.headers.authorization.split(' ')[1];
+ }
+ // make sure that the token exists
+ if(!token){
+   return next(new ErrorRes('Not aurhorized', 401));
+ }
+
+ // extract payload from the jwt token
+ try {
+   const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+   console.log(decoded.id)
+    req.user = await Users.findById(decoded.id);
+    next();
+ } catch (error) {
+     next(new ErrorRes('Not aurhorized', 401))
+ }
+})
