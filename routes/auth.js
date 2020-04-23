e445f08fab400b6f473a5d5f3015c33b154de351
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 23:44:00 -0600

    signin route

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  register
+  register,
+  login
 } = require('../controllers/auth');
 
 router.route('/signup')
       .post(register)
-      
+
+router.route('/signin')
+      .post(login);
+
 module.exports = router;
