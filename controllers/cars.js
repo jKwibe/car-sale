69c54db71585099513380d2bd82c84df92996bd3
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 18:04:47 -0600

    clean the controller by removing the try catch block

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,3 +1,5 @@
+const asyncHandler = require('express-async-handler');
+
 const Cars = require('../models/Cars');
 
 const ErrorRes = require('../utilities/error');
@@ -22,68 +24,56 @@ exports.getAllCars = async (req, res, next)=>{
 // @desc    GET /api/v1/car-sales/:id
 // @access  Public
 
-exports.getSingleCar = async (req, res, next) => {
-  try {
-      const car = await Cars.findById(req.params.id)
+exports.getSingleCar = asyncHandler(async (req, res, next) => {
+      const car = await Cars.findById(req.params.id);
+
       if (!car){
       return next(new ErrorRes(`Car not found with the id ${req.params.id}`, 400))
       }
+
       res.status(200).json({
         success: true,
         data: car
       });
-    
-  } catch (error) {
-    next(error)
-  }
-};
+
+});
 
 // @desc    Add a car
 // @desc    POST /api/v1/admin/car
 // @access  Private
 
-exports.addCar = async (req, res, next) => {
+exports.addCar = asyncHandler(async (req, res, next) => {
   // request the body for content
   const carInfo = req.body;
 
-  try {
     const cars = await Cars.create(carInfo);
     res.status(201).json({
       success: true,
       data: cars
-
     });
-  
-} catch (error) {
- next(error)
-}
-};
+
+});
 
 // @desc    Delete a car
 // @desc    POST /api/v1/admin/car
 // @access  Private
 
-exports.deleteCar = async(req, res, next)=>{
+exports.deleteCar = asyncHandler(async(req, res, next)=>{
 
-  try {
   const car = await  Cars.findById(req.params.id);
 
   if(!car){
     return next(new ErrorRes(`Car not found with the id ${req.params.id}`, 400))
   }
     car.remove()
-    res.status(200).json({ success: true})
-  } catch (error) {
-   next(error)
-  }
-
-}
+    res.status(200).json({ success: true});
+});
 
 // @desc    Update a car
 // @desc    PUT /api/v1/admin/car
 // @access  Private
 
-exports.updateCar = async (req, res, next) =>{
+exports.updateCar = asyncHandler(async (req, res, next) =>{
   try {
     const car = await Cars.findByIdAndUpdate(req.params.id, req.body, {
       runValidators: true,
@@ -97,4 +87,4 @@ exports.updateCar = async (req, res, next) =>{
   } catch (error) {
     next(error)
   }
-}
+});
