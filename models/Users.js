27d7e271bb6dd34b5e781d5570c0895f5d9cda5a
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 23:10:13 -0600

    Hash the password

--- models/Users.js
+++ models/Users.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const timestamps = require('mongoose-timestamps');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -15,11 +16,11 @@ const userSchema = new Schema({
       required: [true, 'Enter your last name'],
       trim: true
     },
-  userName: {
+  username: {
     type: String,
     required: [true, 'Enter your username'],
     trim: true,
-    unique: [true, 'User name exists'],
+    unique: [true, 'User name exists. Please try another username'],
     minlength: [5, 'Username should be over 5 characters'],
     match:[
       /^[\S]+$/,
@@ -29,6 +30,7 @@ const userSchema = new Schema({
   email:{
     type: String,
     required: [true, 'Please enter your email'],
+    unique: [true, 'Email exists'],
     match: [
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
               'Enter a valid email'],
@@ -48,5 +50,12 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(timestamps);
+
+// hash the password
+userSchema.pre('save', async function(next){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next()
+});
 
 module.exports = mongoose.model('user', userSchema);
