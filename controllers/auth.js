6f834aadc23a7b11be460024b69d8a87ba03d118
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 21:34:46 -0600

    Add a singup controller

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/Users');
+const ErrorRes = require('../utilities/error')
+
+// @desc    Register User
+// @desc    POST /api/v1/my-carmax/signup
+// @access  Public
+
+exports.register = asyncHandler (async (req, res, next)=>{
+  const {firstName, lastName, userName, email, phone, password} = req.body;
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    userName,
+    phone,
+    password
+  });
+
+  res.status(201).json({
+    success: true,
+    data: user
+  })
+})
