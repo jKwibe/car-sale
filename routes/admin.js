6a267872ce153c5139c9a8005d764ba34e581497
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-22 20:27:11 -0600

    Add the admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,18 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllCars,
-  getSingleCar,
-  addCar,
-  deleteCar,
-  updateCar
+ addCar,
+ updateCar,
+ deleteCar
 } = require('../controllers/cars');
 
 router.route('/')
-        .get(getAllCars)
+      .post(addCar);
+
 
 router.route('/:id')
-      .get(getSingleCar)
-
+      .delete(deleteCar)
+      .put(updateCar);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const express = require('express');
 const dotenv  = require('dotenv');
 const logger  = require('morgan');
 const colors = require('colors');
+const helmet = require('helmet');
 
 const app     = express();
 
-const errorHandler = require('./middleware/error')
+const errorHandler = require('./middleware/error');
 
 dotenv.config({path: './config/config.env'});
+
 
 //import the database connection
 const databaseConnection = require('./config/db');
 
-const carsRoutes = require('./routes/cars');
+const carsRoutes =  require('./routes/cars');
+const adminRoutes = require('./routes/admin');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
 app.use(express.json());
+app.use(helmet());
 
 databaseConnection();
 
@@ -25,7 +30,8 @@ if(process.env.NODE_ENV === 'development'){
     app.use(logger('dev'))
 }
 
-app.use(carsRoutes);
+app.use('/api/v1/cars',carsRoutes);
+app.use('/api/v1/admin',adminRoutes);
 
 //Error Handler
 app.use(errorHandler)
