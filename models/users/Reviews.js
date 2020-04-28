0daf5d047dbbce9050cdd3a111ec2b9cb57d226c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-28 00:14:36 -0600

    Add the review schema

---
+++ models/users/Reviews.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamps');
+
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  title:{
+    type: String,
+    required: [true, 'Add a review title'],
+    maxlength: [20, 'Should have not more than 20 characters']
+  },
+  description:{
+    type: String,
+    required: [true, 'Should have a description'],
+    maxlength:[500, 'Should not be more than 500 characters']
+  },
+  user:{
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  }
+});
+reviewSchema.plugin(timestamps);
+
+module.exports = mongoose.model('review', reviewSchema);
